3a85ad6b288029b3b941cfc5c903155507ea22fe
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2021-12-08 00:06:16 -0300

    Unificación de estilo de código y configuración de prettier y eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-};
+  rules: {
+    semi: [0, 'never', { beforeStatementContinuationChars: 'always' }],
+    'comma-dangle': [0, 'never']
+  }
+}
