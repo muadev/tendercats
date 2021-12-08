c44d87b4252a44d5c165d72586f12e268feb1d03
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2021-12-08 01:22:08 -0300

    Herramientas de estilo de código (#1)
    * Arreglar opción obsoleta
    
    * Unificación de estilo de código y configuración de prettier y eslint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'none',
   arrowParens: 'avoid',
-};
+  semi: false
+}
