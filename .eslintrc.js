828e8b88cab9c0bba30c6def8b64b5d455975c5e
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2021-12-21 23:48:36 -0300

    Algunas reglas de eslint y mejor interacción con prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,14 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   rules: {
-    semi: [0, 'never', { beforeStatementContinuationChars: 'always' }],
-    'comma-dangle': [0, 'never']
+    // Desactivar las reglas de prettier porque de todas maneras lo estamos
+    // usando antes.
+    'prettier/prettier': 0,
+    // Nunca usar ; al final de las líneas.
+    'semi': [1, 'never', { beforeStatementContinuationChars: 'always' }],
+    // Nunca dejar una , al final de los items en arrays y objetos.
+    'comma-dangle': [1, 'never'],
+    // Forzar a que haya espacios entre { } incluso en jsx.
+    'react/jsx-curly-spacing': [1, { when: 'always', children: { when: 'always' } }]
   }
 }
