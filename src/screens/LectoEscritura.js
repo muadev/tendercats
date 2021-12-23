c9fb90ab500baab25a8680593aaadc41ca6ddba7
ta <tes@riseup.net>
2021-12-23 03:42:08 +0100

    Lectoescritura de Firebase con hooks.

--- src/screens/LectoEscritura.js
+++ src/screens/LectoEscritura.js
@@ -1,21 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { Text, TextInput, View, Button } from 'react-native'
 import database from '@react-native-firebase/database'
+import { DatabaseContext } from '../context/Database'
 
 const LectoEscritura = ({ navigation }) => {
   const [gato, setGato] = useState('Estado de un gato')
+  // Hay que pedir el contexto dentro del componente y no en useEffect().
+  const db = useContext(DatabaseContext)
 
   useEffect(() => {
-    database()
-      .ref('/gatxs/')
+    // TODO, extraer referencia desde el componente a un archivo de queries.
+    db.ref(`gatites/0`)
       .on('value', snapshot => {
-        console.log(snapshot.val())
-        setGato(snapshot.val() && snapshot.val()['gato11'])
+        setGato(snapshot.val().nombre)
       })
   })
 
-  const update = text => {
-    database().ref('/gatxs').update({ gato11: text })
+  const update = (id, text) => {
+    // TODO, extraer referencia desde el componente a un archivo de queries.
+    db.ref(`gatites/${id}`).update({ nombre: text })
   }
 
   return (
@@ -27,7 +30,7 @@ const LectoEscritura = ({ navigation }) => {
       <TextInput
         style={{ height: 40 }}
         placeholder="Ingresa un valor para Gato"
-        onChangeText={text => update(text)}
+        onChangeText={text => update(0, text)}
       />
       <Text>{gato}</Text>
       <Button
