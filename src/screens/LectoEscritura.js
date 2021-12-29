585f6c9bdbbf95fe4380b99a58d162ad77e16fa0
nicolaslarrea <larrea.nicolas@gmail.com>
2021-12-29 18:20:25 -0300

    Comentarios de los cambios

--- src/screens/LectoEscritura.js
+++ src/screens/LectoEscritura.js
@@ -1,28 +1,31 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Text, TextInput, View, Button } from 'react-native'
-import database from '@react-native-firebase/database'
 import { DatabaseContext } from '../context/Database'
 
 const LectoEscritura = ({ navigation }) => {
-  const [gato, setGato] = useState('Estado de un gato')
+  const [gato, setGato] = useState('Buscando gati..')
   // Hay que pedir el contexto dentro del componente y no en useEffect().
   const db = useContext(DatabaseContext)
 
   useEffect(() => {
     // TODO, extraer referencia desde el componente a un archivo de queries.
+    // Cambiamos de .on a .once para leer una sola vez la DB hasta que hagamos un update.
     db.ref(`gatites/0`).once('value', snapshot => {
-      setGato(snapshot.val().nombre)
+      // Los signos de pregunta habilitan a que cualquier intermediario sea null.
+      setGato(snapshot?.val()?.nombre)
     })
   })
 
   const update = (id, text) => {
     // TODO, extraer referencia desde el componente a un archivo de queries.
     db.ref(`gatites/${id}`).update({ nombre: text })
+    // Como estamos desacoplado de la DB tenemos que actualizar el estado manualmente.
     setGato(text)
   }
 
   return (
     <View>
+      {/* Usamos defaultValue para que se inicialice el elemento con el valor default mientras busca el de la DB. Si es null aparece el placeholder. */}
       <TextInput
         style={{ height: 40 }}
         placeholder="Ingresa un valor para Gato"
