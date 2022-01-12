b9710b792430b0d771ea62f37d5104cf1281f9ae
ta <tes@riseup.net>
2022-01-12 22:59:28 +0100

    Creación de rama de trabajo y dummy Screen inicial.

---
+++ src/screens/SubirFoto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const SubirFoto = () => {
+    return (
+        <View>
+            <Text>Subir foto aqui:</Text>
+        </View>
+    )
+}
+
+export default SubirFoto
