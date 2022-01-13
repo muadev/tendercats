02228a955e16f0b5e348534e3ab417d7f54624da
ta <tes@riseup.net>
2022-01-13 02:03:13 +0100

    Probando lib react-native-image-picker.

--- src/screens/SubirFoto.js
+++ src/screens/SubirFoto.js
@@ -10,34 +10,6 @@ const SubirFoto = () => {
     console.log('Response = ', response)
   }
 
-  const selectImage = () => {
-    console.log('echoed')
-    const options = {
-      maxWidth: 2000,
-      maxHeight: 2000,
-      storageOptions: {
-        skipBackup: true,
-        path: 'images'
-      }
-    }
-
-    ImagePicker.showImagePicker(options, response => {
-      console.log('Response = ', response)
-
-      if (response.didCancel) {
-        console.log('User cancelled image picker')
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error)
-      } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton)
-      } else {
-        const source = { uri: response.uri }
-        console.log(source)
-        setImage(source)
-      }
-    })
-  }
-
   return (
     <View>
       <Button title="Elegi una imagen" onPress={openPicker} />
