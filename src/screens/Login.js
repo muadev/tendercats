2f16bd136dc2e921c07e32213c075d63e0b76bc1
nicolaslarrea <larrea.nicolas@gmail.com>
2022-02-09 23:56:41 -0300

    Agrega coherencia a Login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, TextInput } from 'react-native'
 import { Text, Button } from 'react-native-paper'
 import auth from '@react-native-firebase/auth'
 
 const Login = ({ navigation }) => {
-  const [mail, onChangeMail] = React.useState(null)
-  const [password, onChangePassword] = React.useState(null)
+  const [email, setEmail] = useState(null)
+  const [password, serPassword] = useState(null)
 
   // TODO, Hacer los inputs `required`.
   return (
@@ -16,22 +16,22 @@ const Login = ({ navigation }) => {
         placeholder="Ingresa tu mail.."
         keyboardType="email-address"
         maxLength={ 64 }
-        onChangeText={ onChangeMail }
-        value={ mail }
+        onChangeText={ setEmail }
+        value={ email }
       />
 
       <TextInput
         placeholder="Ingresa tu contraseña.."
         secureTextEntry={ true }
         maxLength={ 64 }
-        onChangeText={ onChangePassword }
+        onChangeText={ setPassword }
         value={ password }
       />
 
       <Button
         onPress={ () => {
           auth()
-            .signInWithEmailAndPassword(mail, password)
+            .signInWithEmailAndPassword(email, password)
             .then(() => {
               // Acá no tenemos que llamar a ningún .navigate porque al
               // loguearnos se cambia el AuthState y por lo tanto se rerenderiza
