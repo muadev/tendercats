78e223bee9a126771f1fc1ae21be26847ffd763b
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2022-03-23 22:05:04 -0300

    Mocks de algunas librerías externas

---
+++ __mocks__/react-native-image-picker.js
@@ -0,0 +1,8 @@
+// Mock de las funciones que importamos de react-native-image-picker.
+const launchImageLibrary = jest.fn()
+const launchCamera = jest.fn()
+
+export {
+  launchImageLibrary,
+  launchCamera
+}
---
+++ __mocks__/react-native-iphone-x-helper.js
@@ -0,0 +1,7 @@
+const getBottomSpace = jest.fn()
+const getStatusBarHeight = jest.fn()
+
+export {
+  getBottomSpace,
+  getStatusBarHeight
+}
