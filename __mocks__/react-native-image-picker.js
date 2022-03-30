4097b48fc97a05cc0097b5bd2ff08fe3adb787a6
ta <tes@riseup.net>
2022-03-30 22:45:51 +0200

    Borra el import de la libreria react-native-image-picker que reemplazamos por otra.

--- __mocks__/react-native-image-picker.js
+++
@@ -1,8 +0,0 @@
-// Mock de las funciones que importamos de react-native-image-picker.
-const launchImageLibrary = jest.fn()
-const launchCamera = jest.fn()
-
-export {
-  launchImageLibrary,
-  launchCamera
-}
