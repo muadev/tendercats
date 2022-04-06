11ce3ca72722e6c9f3a2ad520d0c8eefd93007ea
nicolaslarrea <larrea.nicolas@gmail.com>
2022-04-06 19:49:02 -0300

    Hace campos email, password y nombre requeridos en pantalla Registro

--- src/screens/Registro.js
+++ src/screens/Registro.js
@@ -49,6 +49,7 @@ const Registro = ({ navigation }) => {
       />
 
       <Button
+        disabled= { email && password && nombre ? false : true}
         onPress={ () => {
           auth()
             .createUserWithEmailAndPassword(email, password)
