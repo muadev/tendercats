fdd28add5aefdbc5ef6108f234e978626fda317d
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2022-07-27 20:41:24 -0300

    No capitalizar ningún input

--- src/componentes/TextInputConError.js
+++ src/componentes/TextInputConError.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import { HelperText, TextInput } from 'react-native-paper'
+import { HelperText } from 'react-native-paper'
 import errores from 'assets/strings/errores'
+
+import TextInputStandard from 'componentes/TextInputStandard'
 
 // Recibe tipos de error y el error en sí para el manejo del HelperText,
 // y además cualquier otra props que vaya para el componente TextInput.
 const TextInputConError = ({ tiposDeError, error, ...props }) => {
   return (
     <>
-      <TextInput
-        mode="outlined"
+      <TextInputStandard
         { ...props }
       />
 
